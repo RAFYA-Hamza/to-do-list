1bea1feea22cdf5fadc3302f929d9642e6981f2b
RYham <rafyahamza99@gmail.com>
2024-10-16 22:27:41 +0100

    Fix some bugs

--- data/toDoLists.js
+++ data/toDoLists.js
@@ -130,31 +130,3 @@ export function modifyToDo(toDoId, taskId, checkedStatus, value) {
 
   saveListsToStorage();
 }
-
-// export let lists = [
-//   {
-//     id: "0",
-//     TitleList: "List 0",
-//     toDo: ["List 0 to-do 0", "List 0 to-do 0", "List 0 to-do 0"],
-//   },
-//   {
-//     id: "1",
-//     TitleList: "List 1",
-//     toDo: [
-//       "List 1 to-do 1",
-//       "List 1 to-do 1",
-//       "List 1 to-do 1",
-//       "List 1 to-do 1",
-//     ],
-//   },
-//   {
-//     id: "2",
-//     TitleList: "List 2",
-//     toDo: ["List 2 to-do 2", "List 2 to-do 2", "List 2 to-do 2"],
-//   },
-//   {
-//     id: "3",
-//     TitleList: "List 3",
-//     toDo: ["List 3 to-do 3", "List 3 to-do 3"],
-//   },
-// ];
